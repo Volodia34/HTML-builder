bc18fe56b3fcaafaf6019dbae96555ddcb2ce940
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-01-21 07:11:04 +0200

    feat: complete task 03-files-in-folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,27 +1,19 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-async function listFilesInSecretFolder() {
-  const folderPath = path.join(__dirname, 'secret-folder');
-  console.log(folderPath);
+async function copyDir() {
+  const source = path.join(__dirname, 'files');
+  const destination = path.join(__dirname, 'files-copy');
 
-  try {
-    const files = await fs.readdir(folderPath, { withFileTypes: true });
+  await fs.mkdir(destination, { recursive: true });
 
-    for (const file of files) {
-      if (file.isFile()) {
-        const filePath = path.join(folderPath, file.name);
-        const fileStats = await fs.stat(filePath);
-        const fileSizeKB = (fileStats.size / 1024).toFixed(3);
-        const fileName = path.parse(file.name).name;
-        const fileExtension = path.extname(file.name).slice(1);
+  const items = await fs.readdir(source);
 
-        console.log(`${fileName} - ${fileExtension} - ${fileSizeKB}kb`);
-      }
-    }
-  } catch (error) {
-    console.error('Error reading the secret-folder:', error.message);
+  for (const item of items) {
+    await fs.copyFile(path.join(source, item), path.join(destination, item));
   }
+
+  console.log('Copy completed successfully!');
 }
 
-listFilesInSecretFolder();
+copyDir();
